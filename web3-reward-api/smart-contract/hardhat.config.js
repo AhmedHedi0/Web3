c4a5f1478fa6e39b064f450573626356116e1c12
abidi01 <abidi01@ads.uni-passau.de>
2025-09-07 07:32:05 +0200

    added tests

--- web3-reward-api/smart-contract/hardhat.config.js
+++ web3-reward-api/smart-contract/hardhat.config.js
@@ -1,12 +1,20 @@
-require("@nomicfoundation/hardhat-ethers");
-require("dotenv").config();
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-verify";
+import dotenv from "dotenv";
 
-module.exports = {
+dotenv.config();
+
+const config = {
   solidity: "0.8.20",
   networks: {
     sepolia: {
       url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [`0x${process.env.PRIVATE_KEY}`]
-    }
-  }
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
+
+export default config;
---
+++ web3-reward-api/smart-contract/test/RewardToken.js
@@ -0,0 +1,32 @@
+import pkg from 'hardhat';
+const { ethers } = pkg;
+import { expect } from "chai";
+
+describe("RewardToken", function () {
+  let rewardToken;
+  let owner;
+  let user;
+
+  beforeEach(async function () {
+    [owner, user] = await ethers.getSigners();
+    const RewardToken = await ethers.getContractFactory("RewardToken");
+    rewardToken = await RewardToken.deploy();
+    await rewardToken.waitForDeployment();
+  });
+
+  it("Should deploy with correct name and symbol", async function () {
+    expect(await rewardToken.name()).to.equal("RewardToken");
+    expect(await rewardToken.symbol()).to.equal("RWT");
+  });
+
+  it("Should mint initial supply to owner", async function () {
+    const totalSupply = await rewardToken.totalSupply();
+    expect(await rewardToken.balanceOf(owner.address)).to.equal(totalSupply);
+  });
+
+  it("Should allow owner to reward users", async function () {
+    const rewardAmount = ethers.parseEther("100");
+    await rewardToken.reward(user.address, rewardAmount);
+    expect(await rewardToken.balanceOf(user.address)).to.equal(rewardAmount);
+  });
+});
